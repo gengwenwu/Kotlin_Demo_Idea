3ca7661da0d018a8433779daa7d5d6814c22ec2a
Logan <490211581@qq.com>
2019-12-12 18:41:51 +0800

    完善注释

--- src/org/logan/test/kotlin/basic/cp07/7.3.1_1_IndexOperator.kt
+++ src/org/logan/test/kotlin/basic/cp07/7.3.1_1_IndexOperator.kt
@@ -1,8 +1,8 @@
 package org.logan.test.kotlin.basic.cp07
 
 /**
- * desc: 通过下标来访问元素：“get”  <br/>
- * 语法 a[b] (称为：下标运算符)
+ * desc: 下标运算符的预定 -- 通过下标来访问元素：“get”  <br/>
+ *        语法 a[b] 称为：下标运算符
  *
  *  定义一个名为 get 的函数，并标记 operator，就可以使用下标运算符调用了。<br/>
  *  get函数的参数可以是任何类型，任意个数。也可以定义多个重载的 get方法。<br/>
