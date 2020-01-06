d28894830b427c35f4e62c72080e6b4b7956356c
Logan <490211581@qq.com>
2020-01-06 16:59:22 +0800

    匿名函数代替局部返回语法

--- src/org/logan/test/kotlin/basic/cp08/8.3.3_AnonymousFunctions.kt
+++ src/org/logan/test/kotlin/basic/cp08/8.3.3_AnonymousFunctions.kt
@@ -7,6 +7,9 @@ import org.logan.test.kotlin.basic.cp08.ext8311.people
  * desc: 匿名函数: 默认使用局部返回 <br/>
  *       匿名函数看起来跟普通函数很相似，除了它的名字和参数类型被省略了外。
  *
+ *       匿名函数给 lambda表达式提供了另一种可选的语法，用不同的规则来解析 return 表达式。
+ *       可以在需要编写有多个退出点的代码块的时候使用它们。
+ *
  * time: 2020/1/6 4:38 下午 <br/>
  * author: Logan <br/>
  * since V 1.0 <br/>
@@ -37,6 +40,20 @@ fun useAnonymousFunctionInFilter() {
     }).let {
         println(it)
     }
+
+
+    // 在匿名函数中，不带标签的 return 表达式会从匿名函数返回 ，而不是从包含匿名函数的函数返回。
+    // 这条规则很简单: return 从最近的使用 fun 关键字声明的函数返回。
+    println(people.filter(fun(person) = person.age < 30))
+
+    // return 返回比较：
+    //    lambda表达式没有使用 fun 关键字，所以 lambda中的 return 从最外层的函数返回。
+    //    匿名函数使用了fun， 所以， return 表达式从匿名函数返回，而不是从最外层的函数返回。
+
+
+    // 注意
+    // 尽管匿名函数看起来跟普通函数很相似，但它其实是 lambda 表达式的另一种语法形式而已。
+    // 关于 lambda 表达式如何实现，以及在内联函数中如何被内联的讨论同样适用于匿名函数。
 
 }
 
