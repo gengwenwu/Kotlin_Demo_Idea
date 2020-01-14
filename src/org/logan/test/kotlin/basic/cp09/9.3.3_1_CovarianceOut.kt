59046a3588d148281da83132e966b753b76079d9
Logan <490211581@qq.com>
2020-01-14 10:35:40 +0800

    修改注释

--- src/org/logan/test/kotlin/basic/cp09/9.3.3_1_CovarianceOut.kt
+++ src/org/logan/test/kotlin/basic/cp09/9.3.3_1_CovarianceOut.kt
@@ -9,8 +9,8 @@ package org.logan.test.kotlin.basic.cp09
  */
 
 
-// 一个协变类是一个个泛型类(以下面 Producer<T>为例），对这种类来说，下面的描述是成立的：
-// 如果 A 是 B 的子类型，那么 Producer<A>就是 Producer<B>的子类型。我们说子类型化被保留了。
+// 一个协变类是一个泛型类(以下面 Producer<T>为例），对这种类来说，下面的描述是成立的：
+// 如果 A 是 B 的子类型，那么 Producer<A> 就是 Producer<B> 的子类型。我们说子类型化被保留了。
 // 例如 ，Producer<Cat> 是 Producer<Animal> 的子类型， 因为 Cat 是 Animal 的子类型。
 // 在 Kotlin 中，要声明类在某个类型参数上是可以协变的，在该类型参数的名称前加上 out 关键字:
 interface Producer<out T> { // 类被声明在T上的协变
@@ -23,7 +23,7 @@ open class Animal {
     open fun feed() {}
 }
 
-// 将一个类的类型参数标记为协变的，在该 类型实参没有精确匹配到函数中定义的类型形参时，
+// 将一个类的类型参数标记为协变的，在该类型实参 没有精确匹配到函数中定义的类型形参时，
 // 可以让该类的值作为这些函数的实参传递，也可以作为这些函数的返回值。
 class Herd<out T : Animal> { // 使用out，将 T 声明可协变的
     private val animals: List<T> = listOf()
@@ -33,7 +33,7 @@ class Herd<out T : Animal> { // 使用out，将 T 声明可协变的
     fun get(i: Int): T = animals[i] // 把 T 作为返回值使用
 }
 
-fun feedAll(animals: Herd<Animal>) { // 类型参数没有声明协变的
+fun feedAll(animals: Herd<Animal>) {
     for (i in 0 until animals.size) {
         animals.get(i).feed()
     }
@@ -48,7 +48,8 @@ class Cat : Animal() { // Cat是一个Animal
 fun takeCareOfCate(cats: Herd<Cat>) {
     for (i in 0 until cats.size) {
         cats.get(i).cleanLitter()
-        feedAll(cats) // 如果将声明Herd的out删除，将会编译错误：推导的类型是Herd<Cat>，但期望的却是Herd<Animal>
+        feedAll(cats) // 如果将Herd类声明的out删除，将会编译错误：推导的类型是Herd<Cat>，但期望的却是Herd<Animal>
+        //               但是一旦声明为协变的，编译可以通过了。
     }
 }
 
--- src/org/logan/test/kotlin/basic/cp09/9.3.3_2_Covariance.kt
+++ src/org/logan/test/kotlin/basic/cp09/9.3.3_2_Covariance.kt
@@ -5,6 +5,7 @@ package org.logan.test.kotlin.basic.cp09
  *
  * 不能把任何类都变成协变的:这样不安全。
  * 让类在某个类型参数变为协变，限制了该类中对该类型参数使用的可能性。
+ *
  * 要保证类型安全，它只能用在所谓的 out位置，意味着这个类只能生产类型 T 的值而不能消费它们。
  *
  * time: 2020/1/13 2:16 下午 <br/>
@@ -15,17 +16,17 @@ package org.logan.test.kotlin.basic.cp09
 
 // 在类成员的声明中类型参数的使用可以分为 in 位置和 out位置 。
 interface Transformer<T> { // <in T> 或者 <out T>
-    // 类的类型参数前的 out 关键字要求所有使用 T 的方法只能把 T 放在 out 位置，而不能放在in位置。
+    // 类的类型参数前的 out 关键字要求所有使用 T 的方法只能把 T 放在 out 位置(返回值)，而不能放在in位置(入参)。
     // 这个关键宇约束了使用 T 的可能性，这保证了对应子类型关系的安全性。
     // 譬如：9.3.3_1_CovarianceOut.kt中 Herd的get()函数返回值。那是一个 out 位置，可以安全地把类声明成协变的。
     // 如果 Herd<Animal> 类的get方法返回的是Cat，任何调用该方法的代码都可以正常工作， 因为 Cat 是 Animal 的子类型。
 
     // 重申一下，类型参数 T 上的关键宇 out 有两层含义:
-    // (1)，子类型化会被保留 (Herd<Cat> 是 Herd <Animal> 的子类型)
+    // (1)，子类型化会被保留 ( Herd<Cat> 是 Herd <Animal> 的子类型)
     // (2)，T 只能用在out位置
 
 
-    // 下面第一个T是消费类型的T（in的位置）。第二个T是生产类型的T（out位置）。
+    // 下面第一个T是消费类型的T（in的位置），第二个T是生产类型的T（out位置）。
     fun transform(t: T): T
 
 
