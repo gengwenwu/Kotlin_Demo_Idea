f62964eefd944b7ee05798eb7f5cd0bd8d76ee9a
Logan <490211581@qq.com>
2020-01-15 11:51:08 +0800

    修改Kotlin逆变注释

--- src/org/logan/test/kotlin/basic/cp09/9.3.3_2_Covariance.kt
+++ src/org/logan/test/kotlin/basic/cp09/9.3.3_2_Covariance.kt
@@ -18,7 +18,7 @@ import org.logan.test.kotlin.basic.cp09.Animal
 
 // 1，在类成员的声明中类型参数的使用可以分为 in 位置和 out位置 。
 interface Transformer<T> { // <in T> 或者 <out T>
-    // 类的类型参数前的 out 关键字要求所有使用 T 的方法只能把 T 放在 out 位置(返回值)，而不能放在in位置(入参)。
+    // 类的类型参数前的 out 关键字要求所有使用 T 的方法只能把 T 放在 out 位置(返回值)，而不能把 T 放在in位置(入参)。
     // 这个关键宇约束了使用 T 的可能性，这保证了对应子类型关系的安全性。
     // 譬如：9.3.3_1_CovarianceOut.kt中 Herd的get()函数返回值。那是一个 out 位置，可以安全地把类声明成协变的。
     // 如果 Herd<Animal> 类的get方法返回的是Cat，任何调用该方法的代码都可以正常工作， 因为 Cat 是 Animal 的子类型。
@@ -46,7 +46,7 @@ interface List<out T> : Collection<T> {
 
 
 // 3，Kotlin MutableList接口，支持读写，但不能把 MutableList<T> 在它的类型参数上声明成协变的。
-// 因为它既含有接收类型为 T 的值作为参数的方法，也含有返回这种值的方法（T同时出现在 in 和 out 位置上）
+// 因为它既含有接收类型为 T 的值作为参数的方法，也含有返回这种值的方法（ T 同时出现在 in 和 out 位置上）
 // 编译器强制实施了这种限制。
 interface MutableList<T> // 不能声明协变
     : List<T>, MutableCollection<T> {
@@ -55,12 +55,12 @@ interface MutableList<T> // 不能声明协变
 
 }
 
-// 4，构造方法的参数既不在 in位置，也不在 out位置。
+// 4，构造方法的参数既不在 in 位置，也不在 out 位置。
 // 即使类型参数声明成了 out，仍然可以在构造方法参数的声明中使用它，如下：
 class Herd<out T : Animal>(vararg animals: T)
 
 
-// 5，如果在构造方法的参数上使用了关键字 val 和 var，就会自动声明一个getter和一个setter (如果属性是可变的)。
+// 5，如果在构造方法的参数上使用了关键字 val 和 var，就会自动声明一个 getter 和一个 setter (如果属性是可变的)。
 // 因此，对只读属性来说，类型参数用在了 out 位置 ，而可变属性在 out 位置和 in 位置都使用了它:
 class Herd2<T : Animal>( // 这个 T 不能支持协变
     var leadAnimal: T, vararg animals: T // 因为类包含属性 leadAnimal 的 setter，它在 in 位置用到了 T。
@@ -70,6 +70,6 @@ class Herd2<T : Animal>( // 这个 T 不能支持协变
 // 6，位置规则只覆盖了类外部可见的 (public、 protected 和 internal) API。
 // 私有方法的参数既不在 in位置也不在 out位置。变型规则只会防止外部使用者对类的误用，但不会对类自己的实现起作用:
 class Herd3<out T : Animal>(// 支持协变
-    private var leadAnimal: T, // leadAnimal 的 T 不在in和out位置，因为它是private的，所有setter也不会有问题。
+    private var leadAnimal: T, // leadAnimal 的 T 不在in和out位置，因为它是private的，所有setter、getter 也不会有问题。
     vararg animals: T
 )
